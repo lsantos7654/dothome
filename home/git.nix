dd3f03e4923511e8d73e5bb2907e9ae2c8e15b0d
Lucas Santos <lsantos7654@gmail.com>
2026-07-19 01:14:15 -0400

    fix: Use settings instead of deprecated extraConfig

--- home/git.nix
+++ home/git.nix
@@ -3,8 +3,10 @@
 {
   programs.git = {
     enable = true;
-    settings.user.name = "Lucas Santos";
-    settings.user.email = "lsantos7654@gmail.com";
-    extraConfig.credential."https://github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
+    settings = {
+      user.name = "Lucas Santos";
+      user.email = "lsantos7654@gmail.com";
+      credential."https://github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
+    };
   };
 }
