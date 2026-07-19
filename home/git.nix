36c665c763256803acc7f882fa990ac434bb08e1
Lucas Santos <lsantos7654@gmail.com>
2026-07-19 01:08:22 -0400

    fix: Add gh credential helper to git config
    - Use gh auth git-credential for GitHub pushes
    - Avoid KDE Wallet askpass dependency

--- home/git.nix
+++ home/git.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.git = {
     enable = true;
     settings.user.name = "Lucas Santos";
     settings.user.email = "lsantos7654@gmail.com";
+    extraConfig.credential."https://github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
   };
 }
